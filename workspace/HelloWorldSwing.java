94e01f3349254ef56069d071accea9f849a7ed06
SB1809 <sophia.babayev@gmail.com>
2025-10-14 16:45:16 +0000

    C

--- workspace/HelloWorldSwing.java
+++ workspace/HelloWorldSwing.java
@@ -1,4 +1,9 @@
-import javax.swing.*;        
+import javax.swing.*;    
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.Flow; 
+ 
 
 public class HelloWorldSwing {
     /**
@@ -13,9 +18,65 @@ public class HelloWorldSwing {
 
         //Add the ubiquitous "Hello World" label.
         JLabel label = new JLabel("Hello World");
+        JButton button = new JButton("Press Me");
+        frame.setLayout(new FlowLayout());
         frame.getContentPane().add(label);
+        frame.getContentPane().add(button);
+        button.addActionListener(new ActionListener() {
 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                // TODO Auto-generated method stub
+               label.setText("You Did It!");
+            }
+        
+        });
+
+        //  Container pane = gui.getContentPane();
+        //  pane.setLayout(new FlowLayout(FlowLayout.RIGHT,30,30));
+        //     pane.add(new JButton("1"));
+        //     pane.add(new JButton("2"));
+        //     pane.add(new JButton("3"));
+        //     for(int i = 0; i < 20; i++) {
+        //         pane.add(new JButton(Integer.toString(i)));
+        //     }
+
+        JPanel something = new JPanel();
+
+        frame.getContentPane().removeAll();
+        frame.getContentPane().add(something);
+        JButton button2 = new JButton("Meh");
+        button2.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+               label.setText("why");
+            }
+        
+        });
+        JButton button3 = new JButton("Ye");
+        button3.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                
+               label.setText("Lie");
+            }
+        
+        });
+        JButton button4 = new JButton("Hi");
+        button4.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+               
+               label.setText("Bye");
+            }
+        
+        });
         //Display the window.
+        
         frame.pack();
         frame.setVisible(true);
     }
